fb0efcb1a94879b6ced7b8eef9a56923aaca6be1
aldhelee Yasser <yaldhelee@gmail.com>
2022-11-21 20:56:36 +0100

    sorted pom plugins, added logging, added logback.xml configuration

--- tutorialapi-server/src/main/java/com/tutorialapi/server/TutorialApiServer.java
+++ tutorialapi-server/src/main/java/com/tutorialapi/server/TutorialApiServer.java
@@ -1,12 +1,15 @@
 package com.tutorialapi.server;
 
 import org.eclipse.jetty.server.HttpConfiguration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import static org.eclipse.jetty.http.HttpScheme.HTTPS;
 
 public class TutorialApiServer {
+    private static final Logger LOGGER = LoggerFactory.getLogger(TutorialApiServer.class);
     public static void main(String[] args){
-        System.out.println("Hello Word!");
+        LOGGER.info("Hello Word!");
 
         HttpConfiguration httpConfiguration = new HttpConfiguration();
         httpConfiguration.setSecureScheme(HTTPS.asString());
