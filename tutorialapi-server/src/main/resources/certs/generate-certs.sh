715d112d8e47687c02033b3d11cfc395b4ef88c1
aldhelee Yasser <yaldhelee@gmail.com>
2022-11-22 22:16:12 +0100

    added keysotre + certs/keys

---
+++ tutorialapi-server/src/main/resources/certs/generate-certs.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+cd "$(dirname $0)"
+
+# Create CA certificate and key
+
+openssl genrsa -out ca.priv 2048
+
+openssl req -x509 -new -nodes -sha256 -days 1825 -key ca.priv \
+        -out ca.crt -subj "/C=DE/ST=Nordrhein-Westfalen/L=Aachen/O=tutorialapi/CN=tutorialapi.com"
+
+# Create server certificate and key
+
+openssl genrsa -out tutorialapi.priv 2048
+
+openssl req -new -key tutorialapi.priv -out tutorialapi.csr \
+        -subj  "/C=DE/ST=Nordrhein-Westfalen/L=Aachen/O=tutorialapi/CN=tutorialapi.com"
+
+
+cat <<EOF >tutorialapi.ext
+subjectAltName = @alt_names
+[alt_names]
+DNS.1 = localhost
+DNS.2 = tutorialapi.com
+DNS.3 = www.tutorialapi.com
+EOF
+
+openssl x509 -req -in tutorialapi.csr -CA ca.crt -CAkey ca.priv \
+        -out tutorialapi.crt -CAcreatesrial -days 1825 -sha256 \
+        -extfile tutorialapi.ext
+
+openssl pkcs12 -export -in tutorialapi.crt -inkey tutorialapi.priv \
+        -out tutorialapi.p12 -certfile tutorialapi.crt \
+        -password pass:lifechange -name tutorialapi
+        
+openssl pkcs12 -in tutorialapi.p12 -out tutorialapi.pub \
+        -clcerts -nokeys -passin pass:lifechange \
+
+openssl pkcs12 -in tutorialapi.p12 -out tutorialapi.pem \
+        -nodes -passin pass:lifechange
